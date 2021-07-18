bd44630ad3613785d3c0d9479d3fb63457e2c009
reriver <N1ceW0rk>
2021-07-18 09:42:01 +0300

    8 alomost

--- main.go
+++ main.go
@@ -11,20 +11,38 @@ import (
 func dirTree(out *os.File, path string, printFiles bool) error {
 	//_, file := filepath.Split(path)
 	//fmt.Println("└───" + file)
-	return scanDir(out, path, printFiles, 0)
+	return scanDir(out, path, printFiles, 0, "")
 }
 
-func scanDir(out *os.File, path string, printFiles bool, shift int) error {
+func scanDir(out *os.File, path string, printFiles bool, shift int, prefix string) error {
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
 		log.Fatal(err)
 		return err
 	}
 
-	filesLeftInDirectory := len(files)
-	//fmt.Println("\n", " Files in " + path + " list: ", filesLeftInDirectory, " shift: ", shift)
+	filesLeftInDirectory := len(files) - 0
+	//fmt.Println("Files in " + path + " list: ", filesLeftInDirectory, " shift: ", shift)
 
 	//err1 := printDir(file, shift)
+
+	for _, file := range files {
+		if file.IsDir() == false { // if file is not a Directory
+			if file.Name() == ".DS_Store" || file.Name() == ".dockerignore" || file.Name() == "dockerfile" || file.Name() == "hw1.md" {
+				filesLeftInDirectory--
+				continue
+			}
+			if !printFiles { // if we need to Print files
+				filesLeftInDirectory--
+			}
+		} else { // if file is Directory
+			if file.Name() == ".git" || file.Name() == ".idea" {
+				filesLeftInDirectory--
+				continue
+			}
+		}
+	}
+	//fmt.Println(prefix, " Files to display in " + path + ": ", filesLeftInDirectory, ", shift: ", shift)
 
 	for _, file := range files {
 
@@ -33,58 +51,77 @@ func scanDir(out *os.File, path string, printFiles bool, shift int) error {
 		if file.IsDir() == false { // if file is not a Directory
 			if file.Name() == ".DS_Store" || file.Name() == ".dockerignore" || file.Name() == "dockerfile" || file.Name() == "hw1.md" {
 				//fmt.Println(" skipping file...")
-				filesLeftInDirectory--
-				fmt.Println("filesLeftInDirectory: ", filesLeftInDirectory)
+				//filesLeftInDirectory--
+				//fmt.Println("filesLeftInDirectory: ", filesLeftInDirectory)
 				continue
 			}
 			if printFiles { // if we need to Print files
-				err := printFile(file, shift)
+				err := printFile(file, shift, filesLeftInDirectory, prefix)
 				if err != nil {
 					return err
 				}
-				filesLeftInDirectory--
-				fmt.Println("filesLeftInDirectory: ", filesLeftInDirectory)
 			}
+			filesLeftInDirectory--
+			//fmt.Println("filesLeftInDirectory: ", filesLeftInDirectory)
 		} else { // if file is Directory
 			//fmt.Println()
 			if file.Name() == ".git" || file.Name() == ".idea" {
 				//fmt.Println(" skipping directory...")
-				filesLeftInDirectory--
-				fmt.Println("filesLeftInDirectory: ", filesLeftInDirectory)
+				//filesLeftInDirectory--
+				//fmt.Println("filesLeftInDirectory: ", filesLeftInDirectory)
 				continue
 			}
-			err1 := printDir(file, shift)
+			err1 := printDir(file, shift, filesLeftInDirectory, prefix)
 			if err1 != nil {
 				return err1
 			}
 			filesLeftInDirectory--
-			fmt.Println("filesLeftInDirectory: ", filesLeftInDirectory)
+			//fmt.Println("filesLeftInDirectory: ", filesLeftInDirectory)
 			//fmt.Println( "Entering new directory; >" + path + "/" + file.Name() + "<")
-			err2 := scanDir(out, path+"/"+file.Name(), printFiles, shift+1)
-			if err2 != nil {
-				return err2
+			if filesLeftInDirectory > -1 {
+				err2 := scanDir(out, path+"/"+file.Name(), printFiles, shift+1, prefix+"│   ")
+				if err2 != nil {
+					return err2
+				}
+			} else {
+				err2 := scanDir(out, path+"/"+file.Name(), printFiles, shift+1, prefix+"    ")
+				if err2 != nil {
+					return err2
+				}
 			}
 		}
 	}
 	return err
 }
 
-func printFile(file fs.FileInfo, shift int) error {
-	//fmt.Println("printFile with shift ", shift)
-	for i := 0; i < shift*1+0; i++ {
-		fmt.Printf("│   ")
+func printFile(file fs.FileInfo, shift int, filesLeftInDirectory int, prefix string) error {
+	//fmt.Println("printFile with shift ", shift, " filesLeftInDirectory ", filesLeftInDirectory)
+	//for i := 0; i < shift*1+0; i++ {
+	//	fmt.Printf("│   ")
+	//}
+	fmt.Printf(prefix)
+	if filesLeftInDirectory > 0 {
+		_, err := fmt.Println("├───"+file.Name(), "\t files to display ", filesLeftInDirectory, "\t shift ", shift)
+		return err
+	} else {
+		_, err := fmt.Println("└───"+file.Name(), "\t files to display ", filesLeftInDirectory, "\t shift ", shift)
+		return err
 	}
-	_, err := fmt.Println("├───" + file.Name()) //, "\t is Dir = ", file.IsDir())
-	return err
 }
 
-func printDir(file fs.FileInfo, shift int) error {
-	//fmt.Println("printDir with shift ", shift)
-	for i := 0; i < shift*1+0; i++ {
-		fmt.Printf("│   ")
+func printDir(file fs.FileInfo, shift int, filesLeftInDirectory int, prefix string) error {
+	//fmt.Println("printDir with shift ", shift, " filesLeftInDirectory ", filesLeftInDirectory)
+	//for i := 0; i < shift*1+0; i++ {
+	//	fmt.Printf("│   ")
+	//}
+	fmt.Printf(prefix)
+	if filesLeftInDirectory > 1 {
+		_, err := fmt.Println("├───"+file.Name(), "\t files to display ", filesLeftInDirectory, "\t shift ", shift)
+		return err
+	} else {
+		_, err := fmt.Println("└───"+file.Name(), "\t files to display ", filesLeftInDirectory, "\t shift ", shift)
+		return err
 	}
-	_, err := fmt.Println("├───" + file.Name()) //, "\t is Dir = ", file.IsDir())
-	return err
 }
 
 func main() {
